ab93b9e4d71f2111879cdcac7295fb4ad5f2c237
Andrew Warren <anwarren@vbi.vt.edu>
2015-02-10 19:27:20 -0500

    adding conversion from gene list to matrix

--- expression_transform.py
+++ expression_transform.py
@@ -15,7 +15,7 @@ import sys
 #experiment_title: "User input", experiment_description: "User input",
 #organism name: "user input", pubmed_id: "user_input"}
 
-#Output
+#Sample Output
 #experiment.json
 #{"origFileName":"filename","geneMapped":4886,"samples":8,"geneTotal":4985,"cdate":"2013-01-28 13:40:47","desc":"user input","organism":"some org","owner":"user name","title":"user input","pmid":"user input","expid":"whatever","collectionType":"ExpressionExperiment","genesMissed":99,"mdate":"2013-01-28 13:40:47"}
 
@@ -28,7 +28,24 @@ import sys
 #sample.json
 #{"sample":[{"sig_log_ratio":2675,"expmean":"1.258","sampleUserGivenId":"LB_stat_AerobicM9_stat_aerobic","expname":"LB_stat_AerobicM9_stat_aerobic","pid":"8f2e7338-9f04-4ba5-9fe2-5365c857d57fS0","genes":4429,"sig_z_score":139,"expstddev":"1.483"}]}
 
+def gene_list_to_matrix(cur_table):
+    comparisons=set(cur_table['Comparison ID'])
+    genes=set(cur_table['Gene ID'])
+    result=pd.DataFrame(index=sorted(list(genes)), columns=sorted(list(comparisons)))
+    gene_pos=cur_table.columns.get_loc('Gene ID')
+    comparison_pos=cur_table.columns.get_loc('Comparison ID')
+    ratio_pos=cur_table.columns.get_loc('Log Ratio')
+    for row in cur_table.iterrows():
+        gene_id=row[-1][gene_pos]
+        comp=row[-1][comparison_pos]
+        ratio=row[-1][ratio_pos]
+        result[comp][gene_id]=ratio
+    return result
+        
+
 def main():
+    matrix_columns=['Gene ID']
+    list_columns=['Gene ID', 'Comparison ID', 'Log Ratio']
     parser = argparse.ArgumentParser()
     parser.add_argument('-x', required=True, help='comparisons file')
     parser.add_argument('--xformat', required=True, help='format of comparisons', choices=['csv', 'tsv', 'xls', 'xlsx'])
@@ -45,9 +62,37 @@ def main():
         sys.stderr.write("Expression transformation: (file,format) pair must be given\n")
         sys.exit(2)
 
+    #read comparisons file
     comparisons_table=None
-    if args.x != None and args.xformat!=None:
-        comparisons_table=None
+    if args.xformat == 'csv':
+        comparisons_table=pd.read_csv(args.x, header=0)
+    if args.xformat == 'tsv':
+        comparisons_table=pd.read_table(args.x, header=0)
+    if args.xformat == 'xls' or args.xformat == 'xlsx':
+        comparisons_table=pd.io.excel.read_excel(args.x, 0, index_col=None, na_values=['NA'])
+
+    check_columns=None
+    if args.xsetup == 'gene_matrix':
+        check_columns=matrix_columns
+    else:
+        check_columns=list_columns
+    columns_ok = True
+    for i in check_columns:
+        columns_ok=columns_ok and i in comparisons_table.columns
+    if not columns_ok:
+            sys.stderr.write("Missing appropriate column names in "+args.xsetup+"\n")
+            sys.exit(2)
+
+    #parse user provided data
+    form_data=None
+    try:
+        form_data=json.loads(args.u)
+    except:
+        sys.stderr.write("Failed to parse user provided form data "+args.u+"\n")
+        raise
+
+    
+
     
      
 if __name__ == "__main__":
