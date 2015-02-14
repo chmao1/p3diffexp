138cd67cc57cec219149a3ecd677f26ee88673a2
Andrew Warren <anwarren@vbi.vt.edu>
2015-02-14 15:41:43 -0500

    updates to make column names less breakable. but they are still required

--- expression_transform.py
+++ expression_transform.py
@@ -61,13 +61,13 @@ def pretty_print_POST(req):
 #convert gene list format to gene matrix
 #there is definitely a more efficient conversion than this...
 def gene_list_to_matrix(cur_table):
-    comparisons=set(cur_table['Comparison ID'])
-    genes=set(cur_table['Gene ID'])
+    comparisons=set(cur_table['sampleUserGivenId'])
+    genes=set(cur_table['exp_locus_tag'])
     result=pd.DataFrame(index=sorted(list(genes)), columns=sorted(list(comparisons)))
-    result['Gene ID']=result.index
-    gene_pos=cur_table.columns.get_loc('Gene ID')
-    comparison_pos=cur_table.columns.get_loc('Comparison ID')
-    ratio_pos=cur_table.columns.get_loc('Log Ratio')
+    result['exp_locus_tag']=result.index
+    gene_pos=cur_table.columns.get_loc('exp_locus_tag')
+    comparison_pos=cur_table.columns.get_loc('sampleUserGivenId')
+    ratio_pos=cur_table.columns.get_loc('log_ratio')
     for row in cur_table.iterrows():
         gene_id=row[-1][gene_pos]
         comp=row[-1][comparison_pos]
@@ -75,11 +75,72 @@ def gene_list_to_matrix(cur_table):
         result[comp][gene_id]=ratio
     return result
 
-def list_to_mapping_table(cur_table):
-    genes=set(cur_table['Gene ID'])
-    result=pd.DataFrame(index=sorted(list(genes)))
-    result['Gene ID']=result.index
+#convert gene matrix format to gene list
+#there is definitely a more efficient conversion than this...
+def gene_matrix_to_list(cur_table):
+    result=pd.melt(cur_table, id_vars=['exp_locus_tag'], var_name='sampleUserGivenId', value_name='log_ratio')
     return result
+
+def list_to_mapping_table(cur_table):
+    genes=set(cur_table['exp_locus_tag'])
+    result=pd.DataFrame(index=sorted(list(genes)))
+    result['exp_locus_tag']=result.index
+    return result
+
+#deal with weird naming of columns.
+def fix_headers(cur_table, target_setup, die):
+    matrix_columns=['gene_id']
+    list_columns=['gene_id', 'comparison_id', 'log_ratio']
+    template_columns=["comparison_id","title","pubmed","accession","organism","strain","gene_modification","experiment_condition","time_point"]
+    check_columns=None
+    limit_columns=True
+    if target_setup == 'gene_matrix':
+        check_columns=matrix_columns
+        limit_columns=False
+        rename={'gene_id': 'exp_locus_tag'}
+    elif target_setup == 'gene_list':
+        check_columns=list_columns
+        rename={'comparison_id':'sampleUserGivenId','gene_id': 'exp_locus_tag'}
+    elif target_setup == 'template':
+        check_columns=template_columns
+        rename={'comparison_id':'sampleUserGivenId', 'title':'expname', 'gene_modification':'mutant', 'experiment_condition':'condition', 'time_point':'timepoint'}
+    else:
+        sys.stderr.write("unrecognized setup "+target_setup+"\n")
+        if die: sys.exit(2)
+    def fix_name(x): return ' '.join(x.split()).strip().lower().replace(" ","_")
+    cur_table.columns=[fix_name(x) if fix_name(x) in check_columns else x for x in cur_table.columns]
+    #patrics downloadable template is not consistent with its help info
+    if 'gene_ids' in cur_table.columns:
+        cur_table=cur_table.rename(columns={'gene_ids':'gene_id'})
+    columns_ok = True
+    for i in check_columns:
+        columns_ok=columns_ok and i in cur_table.columns
+    if not columns_ok:
+            sys.stderr.write("Missing appropriate column names in "+target_setup+"\n")
+            if die: sys.exit(2)
+    if limit_columns:
+        cur_table=cur_table[check_columns]
+    if rename:
+       cur_table=cur_table.rename(columns=rename)
+    return cur_table
+
+#read in the comparisons data and metadata
+def process_table(target_file, target_format, target_setup, die):
+    if not os.path.exists(target_file):
+        sys.stderr.write("can't find target file "+target_file+"\n")
+        if die: sys.exit(2)
+    cur_table=None
+    if target_format == 'csv':
+        cur_table=pd.read_csv(target_file, header=0)
+    elif target_format == 'tsv':
+        cur_table=pd.read_table(target_file, header=0)
+    elif target_format == 'xls' or target_format == 'xlsx':
+        cur_table=pd.io.excel.read_excel(target_file, 0, index_col=None)
+    else:
+        sys.stderr.write("unrecognized format "+target_format+" for "+target_setup+"\n")
+        if die: sys.exit(2)
+    cur_table=fix_headers(cur_table, target_setup, die)
+    return cur_table
 
 #{source_id_type:"refseq_locus_tag || alt_locus_tag || feature_id", 
 #data_type: "Transcriptomics || Proteomics || Phenomics", 
@@ -109,50 +170,42 @@ def create_comparison_files(output_path, comparisons_table, form_data, experimen
     sample_dict={'sample':[]}
     expression_dict={'expression':[]}
     #create stats table for sample.json
-    grouped=comparisons_table.groupby(["Comparison ID"])
-    sample_stats=grouped.agg([np.mean, np.std])['Log Ratio']
+    grouped=comparisons_table.groupby(["sampleUserGivenId"])
+    sample_stats=grouped.agg([np.mean, np.std])['log_ratio']
     sample_stats=sample_stats.rename(columns={'mean':'expmean','std':'expstddev'})
-    sample_stats["genes"]=grouped.count()["Gene ID"]
+    sample_stats["genes"]=grouped.count()["exp_locus_tag"]
     sample_stats["pid"]=[str(experiment_id)+"S"+str(i) for i in range(0,len(sample_stats))]
     sample_stats["sampleUserGivenId"]=sample_stats.index
     sample_stats["expname"]=sample_stats.index
     #get zscore and significance columns
     comparisons_table["z_score"]=grouped.transform(stats.zscore)
     comparisons_table["sig_z"]=comparisons_table["z_score"] >= sig_z
-    comparisons_table["sig_log"]=comparisons_table["Log Ratio"] >= sig_log
+    comparisons_table["sig_log"]=comparisons_table["log_ratio"] >= sig_log
     #store counts in stats
-    sample_stats["sig_z_score"]=comparisons_table.groupby(["Comparison ID","sig_z"]).count()['sig_z'].unstack()[True]
-    sample_stats["sig_log_ratio"]=comparisons_table.groupby(["Comparison ID","sig_log"]).count()['sig_log'].unstack()[True]
+    sample_stats["sig_z_score"]=comparisons_table.groupby(["sampleUserGivenId","sig_z"]).count()['sig_z'].unstack()[True]
+    sample_stats["sig_log_ratio"]=comparisons_table.groupby(["sampleUserGivenId","sig_log"]).count()['sig_log'].unstack()[True]
     sample_stats["sig_log_ratio"]=sample_stats["sig_log_ratio"].fillna(0).astype('int64')
     sample_stats["sig_z_score"]=sample_stats["sig_z_score"].fillna(0).astype('int64')
     #set pid's for expression.json
-    comparisons_table=comparisons_table.rename(columns={'Comparison ID':'sampleUserGivenId','Gene ID': 'exp_locus_tag', 'Map ID': "feature_id","Log Ratio":'log_ratio'})
     comparisons_table=comparisons_table.merge(sample_stats[["pid","sampleUserGivenId"]], how="left", on="sampleUserGivenId")
     #pull in metadata spreadsheet if provided
     if 'metadata_template' in form_data and 'metadata_format' in form_data and form_data['metadata_template']:
-        meta_table=None
-        meta_cols=["Comparison ID","Title","PubMed","Accession","Organism","Strain","Gene Modification","Experiment Condition","Time Point"]
+        meta_table=process_table(form_data['metadata_template'], form_data['metadata_format'], target_setup='template', die=True)
         try:
-            if form_data['metadata_format'] == 'xls' or form_data['metadata_format'] == 'xlsx':
-                meta_table=pd.io.excel.read_excel(form_data['metadata_template'], 0, index_col=None)
-            elif form_data['metadata_format'] == 'csv':        
-                meta_table=pd.read_csv(form_data['metadata_template'], header=0)
-            else:
-                meta_table=pd.read_table(form_data['metadata_template'], header=0)
             meta_key="sampleUserGivenId"
-            meta_table=meta_table[meta_cols].rename(columns={'Comparison ID':'sampleUserGivenId', 'Title':'expname', 'PubMed':'pubmed', 'Accession':'accession', 'Organism':'organism', 'Strain':'strain', 'Gene Modification':'mutant', 'Experiment Condition':'condition', 'Time Point':'timepoint'})
             to_add=meta_table.columns-sample_stats.columns
             meta_table=meta_table.set_index('sampleUserGivenId')
             sample_stats.update(meta_table)
-            sample_stats=sample_stats.merge(meta_table[to_add], left_index=True, right_index=True)
+            sample_stats=sample_stats.merge(meta_table[to_add], left_index=True, right_index=True, how='left')
         except:
-            sys.stderr.write("failed to use user provide metadata template\n")
-            pass
+            sys.stderr.write("failed to parse user provide metadata template\n")
+            sys.exit(2)
     #populate json dicts
     sample_stats=sample_stats.fillna("")
-    sample_dict['sample']=sample_stats.to_dict(outtype='records')
+    sample_dict['sample']=json.loads(sample_stats.to_json(orient='records', date_format='iso'))
+    #sample_dict['sample']=sample_stats.to_dict(outtype='records')
     cols = [col for col in comparisons_table.columns if col not in ['sig_z', 'sig_log']]
-    expression_dict['expression']=comparisons_table[cols].to_dict(outtype='records')
+    expression_dict['expression']=json.loads(comparisons_table[cols].to_json(orient='records'))
     output_file=os.path.join(output_path, 'sample.json')
     out_handle=open(output_file, 'w')
     json.dump(sample_dict, out_handle)
@@ -171,8 +224,8 @@ def create_comparison_files(output_path, comparisons_table, form_data, experimen
 #creates mapping.json for results
 def create_mapping_file(output_path, mapping_table, form_data):
     mapping_dict={"mapping":{"unmapped_list":[],"unmapped_ids":0,"mapped_list":[],"mapped_ids":0}}
-    mapping_dict['mapping']['unmapped_list']=mapping_table[mapping_table.isnull().any(axis=1)][['Gene ID']].rename(columns={'Gene ID': 'exp_locus_tag'}).to_dict(outtype='records')
-    mapping_dict['mapping']['mapped_list']=mapping_table[mapping_table.notnull().all(axis=1)].rename(columns={'Gene ID': 'exp_locus_tag', 'Map ID': "feature_id"}).to_dict(outtype='records')
+    mapping_dict['mapping']['unmapped_list']=mapping_table[mapping_table.isnull().any(axis=1)][['exp_locus_tag']].to_dict(outtype='records')
+    mapping_dict['mapping']['mapped_list']=mapping_table[mapping_table.notnull().all(axis=1)].to_dict(outtype='records')
     mapping_dict['mapping']['unmapped_ids']=len(mapping_dict['mapping']['unmapped_list'])
     mapping_dict['mapping']['mapped_ids']=len(mapping_dict['mapping']['mapped_list'])
     output_file=os.path.join(output_path, 'mapping.json')
@@ -184,11 +237,6 @@ def create_mapping_file(output_path, mapping_table, form_data):
     #mapped_list=[{form_data["source_id_type"]: i["Map ID"], "exp_locus_tag":i['Gene ID']} for i in mapping_table[mapping_table.notnull().any(axis=1)]]
     #mapped_list=[{form_data["source_id_type"]: i["Map ID"], "exp_locus_tag":i["Gene ID"]} for i in mapping_table.query('Gene ID != @np.nan')]
 
-#convert gene matrix format to gene list
-#there is definitely a more efficient conversion than this...
-def gene_matrix_to_list(cur_table):
-    result=pd.melt(cur_table, id_vars=['Gene ID'], var_name='Comparison ID', value_name='Log Ratio')
-    return result
 
 def place_ids(query_results,cur_table,form_data):
     for d in query_results.json()['response']['docs']:
@@ -199,14 +247,15 @@ def place_ids(query_results,cur_table,form_data):
         if 'feature_id' in d:
             target_id=d['feature_id']
         if source_id and target_id:
-            cur_table["Map ID"][source_id]=target_id
+            cur_table["feature_id"][source_id]=target_id
 
 def make_map_query(id_list, form_data, server_setup, chunk_size):
     current_query={'q':form_data["source_id_type"]+":("+" OR ".join(id_list)+") AND annotation:PATRIC"}
     current_query["fl"]="feature_id,"+form_data["source_id_type"]
     current_query["rows"]=str(chunk_size)
     current_query["wt"]="json"
-    headers = {'Content-Type': 'application/solrquery+x-www-form-urlencoded'}
+    print "switch THE HEADER BACK!"
+    headers = {"Content-Type": "application/solrquery+x-www-form-urlencoded"}
     #headers = {'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8'}
     req = requests.Request('POST', server_setup["data_api"], headers=headers, data=current_query)
     prepared = req.prepare()
@@ -222,16 +271,14 @@ def chunker(seq, size):
     return (seq[pos:pos + size] for pos in xrange(0, len(seq), size))
 
 def map_gene_ids(cur_table, form_data, server_setup):
-    cur_table["Map ID"]=np.nan
+    cur_table["feature_id"]=np.nan
     chunk_size=10000
-    for i in chunker(cur_table['Gene ID'], chunk_size):
+    for i in chunker(cur_table['exp_locus_tag'], chunk_size):
         mapping_results=make_map_query(i, form_data, server_setup, chunk_size)
         place_ids(mapping_results, cur_table, form_data)
-        
+
 
 def main():
-    matrix_columns=['Gene ID']
-    list_columns=['Gene ID', 'Comparison ID', 'Log Ratio']
     sig_z=2
     sig_log=1
     valid_formats=set(['csv', 'tsv', 'xls', 'xlsx'])
@@ -278,26 +325,9 @@ def main():
         sys.stderr.write("Expression transformation: (file,format) pair must be given for metadata template\n")
         #sys.exit(2)
 
-    #read comparisons file
-    comparisons_table=None
-    if form_data['xformat'] == 'csv':
-        comparisons_table=pd.read_csv(form_data['xfile'], header=0)
-    if form_data['xformat'] == 'tsv':
-        comparisons_table=pd.read_table(form_data['xfile'], header=0)
-    if form_data['xformat'] == 'xls' or form_data['xformat'] == 'xlsx':
-        comparisons_table=pd.io.excel.read_excel(form_data['xfile'], 0, index_col=None)
 
-    check_columns=None
-    if form_data['xsetup'] == 'gene_matrix':
-        check_columns=matrix_columns
-    else:
-        check_columns=list_columns
-    columns_ok = True
-    for i in check_columns:
-        columns_ok=columns_ok and i in comparisons_table.columns
-    if not columns_ok:
-            sys.stderr.write("Missing appropriate column names in "+form_data['xfile']+"\n")
-            sys.exit(2)
+    #read comparisons file
+    comparisons_table=process_table(form_data['xfile'], form_data['xformat'], form_data['xsetup'], die=True)
 
     output_path=form_data["output_path"]
 
@@ -306,14 +336,14 @@ def main():
         comparisons_table=gene_matrix_to_list(comparisons_table)
 
     #limit log ratios
-    comparisons_table.ix[comparisons_table["Log Ratio"] > 1000000, 'Log Ratio']=1000000
-    comparisons_table.ix[comparisons_table["Log Ratio"] < -1000000, 'Log Ratio']=-1000000
+    comparisons_table.ix[comparisons_table["log_ratio"] > 1000000, 'log_ratio']=1000000
+    comparisons_table.ix[comparisons_table["log_ratio"] < -1000000, 'log_ratio']=-1000000
     comparisons_table=comparisons_table.dropna()
 
     #map gene ids
     mapping_table=list_to_mapping_table(comparisons_table)
     map_gene_ids(mapping_table, form_data, server_setup)
-    comparisons_table=comparisons_table.merge(mapping_table, how='left', on="Gene ID")
+    comparisons_table=comparisons_table.merge(mapping_table, how='left', on='exp_locus_tag')
 
     #create json files to represent experiment
     experiment_id=str(uuid.uuid1())
